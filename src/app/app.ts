cdc36473da8dd7f4656f95ca9a99e2c12aa048de
Deva-2002 <mikkuvakkom391@gmail.com>
2025-11-16 05:31:43 +0530

    created child componenets

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./componenet/header/header";
+import { Main } from "./componenet/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Main],
   template: `
     <h1>Welcome to :{{ title() }}!</h1>
   <p>Just saying random things</p>
+  <app-header/>
+  <app-main/>
     <router-outlet />
   `,
   styles: [`
