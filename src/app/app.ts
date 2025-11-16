a7b21726255b91d0e2a6fbfa73e2e5ffef3a20ef
Deva-2002 <mikkuvakkom391@gmail.com>
2025-11-16 06:58:29 +0530

    routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Header } from "./componenet/header/header";
 import { Main } from "./componenet/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Main],
+  imports: [RouterOutlet, Header, Main,RouterLink],
   template: `
     <h1>Welcome to :{{ title() }}!</h1>
   <p>Just saying random things</p>
+  <p routerLink="/todo">Todo</p>
   <app-header [about]=aboutMessage()/>
   <app-main/>
     <router-outlet />
